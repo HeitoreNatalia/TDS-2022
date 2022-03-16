a9efe1b2c05e1c8b458f06b90ac839e13bac1e67
aluno <aluno@S2C59E5BE849D>
2022-03-16 15:31:32 -0300

    Adicionar arquivos de projeto.

---
+++ Princicpal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Princicpal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
