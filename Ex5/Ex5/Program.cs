baeb7e8d29712e67d42b9d9766fd5975c0806be5
ReiDasCapivaras <githubunicampheitor@g.unicamp.br>
2022-03-22 02:30:24 -0300

    adicionado exercicio5 (ainda precisa modificar algumas coisas)

---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int codigopeca1 = Convert.ToInt32(Console.ReadLine());
+        int npeca1 = Convert.ToInt32(Console.ReadLine());
+        float valorpeca1 = Single.Parse(Console.ReadLine());
+
+        int codigopeca2 = Convert.ToInt32(Console.ReadLine());
+        int npeca2 = Convert.ToInt32(Console.ReadLine());
+        float valorpeca2 = Single.Parse(Console.ReadLine());
+
+        float valor_total = (valorpeca1 * npeca1) + (valorpeca2 * npeca2);
+        Console.WriteLine("VALOR TOTAL A PAGAR: ");
+        Console.WriteLine(valor_total);
+    }
+}
