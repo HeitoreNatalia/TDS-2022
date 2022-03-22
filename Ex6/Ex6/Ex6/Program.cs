192db258d1ca9e2b881ad1b802b60be57aa56d8e
ReiDasCapivaras <githubunicampheitor@g.unicamp.br>
2022-03-22 20:53:44 -0300

    feito exercício 6

---
+++ Ex6/Ex6/Ex6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("");
+        float a = Convert.ToSingle(Console.ReadLine());
+        Console.Write("");
+        float b = Convert.ToSingle(Console.ReadLine());
+        Console.Write("");
+        float c = Convert.ToSingle(Console.ReadLine());
+
+        Console.WriteLine("TRIANGULO: " + (a * c) / 2);
+        Console.WriteLine("CIRCULO: " + (c * c) * 3.14159);
+        Console.WriteLine("TRAPEZIO: " + ((a + b) / 2) * c);
+        Console.WriteLine("QUADRADO: " + (b * b));
+        Console.WriteLine("RETANGULO: " + a * b);
+        Console.ReadLine();
+
+    }
+}
